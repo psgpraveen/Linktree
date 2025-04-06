eb6468a39086aa84d3ec53677742141c86a831c0
Praveen Kumar Gupta <pg23403@gmail.com>
2025-04-06 12:24:58 +0530

    Fix error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    domains: ['lh3.googleusercontent.com'], // 👈 add this line
+  },};
 
 export default nextConfig;
